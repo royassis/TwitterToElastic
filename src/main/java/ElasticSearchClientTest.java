f36734cc92d84ced3ff368d9217753cdc6d734d1
Roy <royassisg@gmail.com>
2021-02-06 16:19:20 +0200

    cmt

---
+++ src/main/java/ElasticSearchClientTest.java
@@ -0,0 +1,32 @@
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+
+public class ElasticSearchClientTest {
+    public static void main(String[] args) throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http"),
+                        new HttpHost("localhost", 9201, "http")));
+
+        IndexRequest request = new IndexRequest("posts");
+        request.id("1");
+        String jsonString = "{" +
+                "\"user\":\"kimchy\"," +
+                "\"postDate\":\"2013-01-30\"," +
+                "\"message\":\"trying out Elasticsearch\"" +
+                "}";
+        request.source(jsonString, XContentType.JSON);
+
+        IndexResponse indexResponse = client.index(request, RequestOptions.DEFAULT);
+
+        client.close();
+
+    }
+}
---
+++ src/main/java/ElasticSearchConsumerWithThread.java
@@ -0,0 +1,150 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+
+public class ElasticSearchConsumerWithThread {
+
+    public static void main(String[] args) {
+        new ElasticSearchConsumerWithThread().run();
+    }
+
+    private ElasticSearchConsumerWithThread() {
+
+    }
+
+    private void run() {
+        Logger logger = LoggerFactory.getLogger(ElasticSearchConsumerWithThread.class.getName());
+
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "my-new-application";
+        String topic = "twitter-streaming-test";
+
+        // latch for dealing with multiple threads
+        CountDownLatch latch = new CountDownLatch(1);
+
+        // create the consumer runnable
+        logger.info("Creating the consumer thread");
+        Runnable myConsumerRunnable = new ConsumerRunnable(
+                bootstrapServers,
+                groupId,
+                topic,
+                latch
+        );
+
+        // start the thread
+        Thread myThread = new Thread(myConsumerRunnable);
+        myThread.start();
+
+        // add a shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Caught shutdown hook");
+            ((ConsumerRunnable) myConsumerRunnable).shutdown();
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            logger.info("Application has exited");
+        }
+
+        ));
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            logger.error("Application got interrupted", e);
+        } finally {
+            logger.info("Application is closing");
+        }
+    }
+
+    public class ConsumerRunnable implements Runnable {
+
+        private CountDownLatch latch;
+        private KafkaConsumer<String, String> consumer;
+        private Logger logger = LoggerFactory.getLogger(ConsumerRunnable.class.getName());
+
+        public ConsumerRunnable(String bootstrapServers,
+                                String groupId,
+                                String topic,
+                                CountDownLatch latch) {
+            this.latch = latch;
+
+            // create consumer configs
+            Properties properties = new Properties();
+            properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+            properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+            properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+            // create consumer
+            consumer = new KafkaConsumer<String, String>(properties);
+            // subscribe consumer to our topic(s)
+            consumer.subscribe(Arrays.asList(topic));
+        }
+
+        @Override
+        public void run() {
+            RestHighLevelClient client = new RestHighLevelClient(
+                    RestClient.builder(
+                            new HttpHost("localhost", 9200, "http"),
+                            new HttpHost("localhost", 9201, "http")));
+
+            // poll for new data
+            try {
+                Integer requestId = 1;
+                while (true) {
+                    ConsumerRecords<String, String> records =
+                            consumer.poll(Duration.ofMillis(100)); // new in Kafka 2.0.0
+
+                    for (ConsumerRecord<String, String> record : records) {
+                        logger.info("requestId: " + requestId.toString() +" Key: " + record.key() + ", Value: " + record.value());
+//                        logger.info("Partition: " + record.partition() + ", Offset:" + record.offset());
+                        Thread.sleep(500);
+
+                        IndexRequest request = new IndexRequest("tweets");
+                        request.id(requestId.toString());
+                        String jsonString = record.value();
+                        request.source(jsonString, XContentType.JSON);
+                        IndexResponse indexResponse = client.index(request, RequestOptions.DEFAULT);
+                        requestId++;
+                    }
+                }
+            } catch (WakeupException | IOException | InterruptedException e) {
+                logger.info("Received shutdown signal!");
+            } finally {
+                consumer.close();
+
+                // tell our main code we're done with the consumer
+                latch.countDown();
+            }
+        }
+
+        public void shutdown() {
+            // the wakeup() method is a special method to interrupt consumer.poll()
+            // it will throw the exception WakeUpException
+            consumer.wakeup();
+        }
+    }
+}
