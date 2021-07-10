6cce93f1142f9e9ae6429eba1e8b14c784f77e1b
Roy <royassisg@gmail.com>
2021-07-10 17:29:08 +0300

    Make small change

--- TwitterToTerminal/src/main/java/TwitterStreamer.java
+++ TwitterToTerminal/src/main/java/TwitterStreamer.java
@@ -20,6 +20,8 @@ import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import java.util.concurrent.TimeUnit;
+
 public class TwitterStreamer {
 
     public static void main(String[] args) throws InterruptedException {
@@ -27,7 +29,7 @@ public class TwitterStreamer {
         BlockingQueue<Event> eventQueue = new LinkedBlockingQueue<Event>(1000);
 
         /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
-        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        Hosts housebirdHosts = new HttpHosts(Constants.STREAM_HOST);
         StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
 
         // Optional: set up some followings and track terms
@@ -51,25 +53,26 @@ public class TwitterStreamer {
 
         ClientBuilder builder = new ClientBuilder()
                 .name("Hosebird-Client-01")                              // optional: mainly for the logs
-                .hosts(hosebirdHosts)
+                .hosts(housebirdHosts)
                 .authentication(hosebirdAuth)
                 .endpoint(hosebirdEndpoint)
                 .processor(new StringDelimitedProcessor(msgQueue))
                 .eventMessageQueue(eventQueue);                          // optional: use this if you want to process client events
 
-        Client hosebirdClient = builder.build();
+        Client housebirdClient = builder.build();
 
         // Attempts to establish a connection.
-        hosebirdClient.connect();
+        housebirdClient.connect();
 
-        while (!hosebirdClient.isDone()) {
+        while (!housebirdClient.isDone()) {
             String msg = msgQueue.take();
             Gson gson = new GsonBuilder().setPrettyPrinting().create();;
             JsonObject jsonObject = gson.fromJson(msg, JsonObject.class);
             System.out.println(gson.toJson(jsonObject));
+            TimeUnit.SECONDS.sleep(1);
         }
 
-        hosebirdClient.stop();
+        housebirdClient.stop();
     }
 
 
